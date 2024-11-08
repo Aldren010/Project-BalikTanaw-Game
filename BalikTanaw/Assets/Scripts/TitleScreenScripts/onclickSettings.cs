8a755d31bc8802668d0f8d2d1441e1c703d480bf
Aldren <busterchester9@gmail.com>
2024-11-08 11:05:08 +0800

    Fixed Titlescreen
    We will now proceed to actual game mechanic and gamemodes

--- BalikTanaw/Assets/Scripts/TitleScreenScripts/onclickSettings.cs
+++ BalikTanaw/Assets/Scripts/TitleScreenScripts/onclickSettings.cs
@@ -1,67 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; // Make sure to include this for Button
+using UnityEngine.UI;
 
 public class OnClickSettings : MonoBehaviour
 {
-    public GameObject settingsPanel; // Reference to the settings panel
-    private Animator animator; // Reference to the Animator component
+    public GameObject settingsPanel;
+    public GameObject buttonsParent; 
+    private Animator animator;
+    private bool isSettingsOpen = false; 
 
     void Start()
     {
         animator = settingsPanel.GetComponent<Animator>();
-        settingsPanel.SetActive(false); // Ensure the panel is hidden at start
+        settingsPanel.SetActive(false); 
     }
 
     public void ToggleSettingsPanel()
     {
-        if (settingsPanel.activeSelf)
+        if (isSettingsOpen)
         {
-            animator.SetTrigger("Close"); // Trigger the close animation
-            Invoke("DeactivatePanel", 0.5f); // Adjust time based on animation length
-            EnableTitleScreenButtons(); // Re-enable buttons when closing
+            CloseSettingsPanel();
         }
         else
         {
-            settingsPanel.SetActive(true); // Show the panel
-            animator.SetTrigger("Open"); // Trigger the open animation
-            DisableTitleScreenButtons(); // Disable buttons when opening
+            OpenSettingsPanel();
         }
+    }
+
+    private void OpenSettingsPanel()
+    {
+        isSettingsOpen = true; 
+        settingsPanel.SetActive(true); 
+        animator.SetTrigger("Open"); 
+        buttonsParent.SetActive(false); 
+    }
+
+    private void CloseSettingsPanel()
+    {
+        isSettingsOpen = false;
+        animator.SetTrigger("Close"); 
+        Invoke("DeactivatePanel", 0.5f); 
     }
 
     private void DeactivatePanel()
     {
-        settingsPanel.SetActive(false); // Hide the panel after closing
-    }
-
-    private void DisableTitleScreenButtons()
-    {
-        // Find all GameObjects with the "TitleButton" tag
-        GameObject[] titleButtonObjects = GameObject.FindGameObjectsWithTag("TitleButton");
-        foreach (GameObject buttonObject in titleButtonObjects)
-        {
-            Button button = buttonObject.GetComponent<Button>();
-            if (button != null)
-            {
-                button.interactable = false; // Disable each button
-                button.gameObject.SetActive(false); // Optionally hide the button
-            }
-        }
-    }
-
-    private void EnableTitleScreenButtons()
-    {
-        // Find all GameObjects with the "TitleButton" tag
-        GameObject[] titleButtonObjects = GameObject.FindGameObjectsWithTag("TitleButton");
-        foreach (GameObject buttonObject in titleButtonObjects)
-        {
-            Button button = buttonObject.GetComponent<Button>();
-            if (button != null)
-            {
-                button.interactable = true; // Enable each button
-                button.gameObject.SetActive(true); // Optionally show the button
-            }
-        }
+        settingsPanel.SetActive(false); 
+        buttonsParent.SetActive(true); 
     }
 }
